86a1636a09cb9f43c57eb4d3504d6e03c2b3904b
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-07-31 15:59:07 +0530

    Create addeven.java

---
+++ addeven.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int res=a+b;
+		if(res%2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
+	}
+}
