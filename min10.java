77c02502954d320c05bfbc3ea3a7df4f04fb1062
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-14 12:31:12 +0530

    Create min10.java

---
+++ min10.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[25];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int min=a[0];
+		for(int i=0;i<10;i++)
+		{
+			if(min>a[i])
+			min=a[i];
+		}
+		System.out.println(min);
+		
+	}
+}
