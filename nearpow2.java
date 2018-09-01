636423a24705a39c894a07152874e877806393f7
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-09-01 18:36:23 +0530

    Create nearpow2.java

---
+++ nearpow2.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=1;
+		for(int i=1;i<n;i++)
+		{
+			p=p*2;
+		}
+		System.out.println(p);
+	}
+}
